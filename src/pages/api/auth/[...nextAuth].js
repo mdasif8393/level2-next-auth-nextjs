c690fcf59bb92b0aa38f01a323f9841fb5652b2d
unknown <md.asif.8393@gmail.com>
2023-12-21 21:55:48 +0600

    [Done-> Add Google Authentication]

--- src/pages/api/auth/[...nextAuth].js
+++ src/pages/api/auth/[...nextAuth].js
@@ -15,6 +15,10 @@ export const authOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-}
+
+    pages: {
+        signIn: "/login"
+    },
+};
 
 export default NextAuth(authOptions)
